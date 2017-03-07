6400ab094a4de89f9227346f888184caddfd3d26
Brad Cornes <bradlc41@gmail.com>
2017-03-07 00:35:19 +0000

    Limit to blade files only

--- src/File/Filesystem.php
+++ src/File/Filesystem.php
@@ -12,6 +12,6 @@ class Filesystem extends BaseFilesystem
 
     public function allFiles($directory, $hidden = false)
     {
-        return iterator_to_array(Finder::create()->ignoreDotFiles($hidden)->files()->in($directory), false);
+        return iterator_to_array(Finder::create()->ignoreDotFiles($hidden)->files()->in($directory)->name('/\.blade\./'), false);
     }
 }
